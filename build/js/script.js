0d6ba76a89b7a7b325b26c61d5eaf315d41ff17c
agfedyashevv <ag.fedyashevv@gmail.com>
2020-10-04 23:23:54 +0300

    header js и без js + ПП + оптимизация изображений

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,21 @@
 'use strict';
 var pageHeader = document.querySelector('.header');
+var main = document.querySelector('.main');
 var headerToggle = document.querySelector('.nav__toggle');
 
 pageHeader.classList.remove('header--nojs');
+main.classList.remove('main--nojs');
 
 headerToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('header--closed')) {
     pageHeader.classList.remove('header--closed');
     pageHeader.classList.add('header--opened');
+    main.classList.remove('main--closed');
+    main.classList.add('main--opened');
   } else {
     pageHeader.classList.add('header--closed');
     pageHeader.classList.remove('header--opened');
+    main.classList.add('main--closed');
+    main.classList.remove('main--opened');
   }
 });
