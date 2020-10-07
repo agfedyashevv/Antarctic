c09c62d3cf324e7ed9efa9c2f07daface35b5354
agfedyashevv <ag.fedyashevv@gmail.com>
2020-10-08 00:44:01 +0300

    js + стилизация кнопки по тегу

--- source/js/script.js
+++ source/js/script.js
@@ -3,19 +3,21 @@ var pageHeader = document.querySelector('.header');
 var main = document.querySelector('.main');
 var headerToggle = document.querySelector('.nav__toggle');
 
-pageHeader.classList.remove('header--nojs');
-main.classList.remove('main--nojs');
+if (pageHeader && main && headerToggle) {
+  pageHeader.classList.remove('header--nojs');
+  main.classList.remove('main--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('header--closed')) {
-    pageHeader.classList.remove('header--closed');
-    pageHeader.classList.add('header--opened');
-    main.classList.remove('main--closed');
-    main.classList.add('main--opened');
-  } else {
-    pageHeader.classList.add('header--closed');
-    pageHeader.classList.remove('header--opened');
-    main.classList.add('main--closed');
-    main.classList.remove('main--opened');
-  }
-});
+  headerToggle.addEventListener('click', function () {
+    if (pageHeader.classList.contains('header--closed')) {
+      pageHeader.classList.remove('header--closed');
+      pageHeader.classList.add('header--opened');
+      main.classList.remove('main--closed');
+      main.classList.add('main--opened');
+    } else {
+      pageHeader.classList.add('header--closed');
+      pageHeader.classList.remove('header--opened');
+      main.classList.add('main--closed');
+      main.classList.remove('main--opened');
+    }
+  });
+}
